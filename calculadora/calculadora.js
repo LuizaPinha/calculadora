6febd79d556760cce929dc68b4ae03be6f794bf4
LuizaPinha <61803894+LuizaPinha@users.noreply.github.com>
2020-09-30 22:11:01 -0300

    Add files via upload

---
+++ calculadora/calculadora.js
@@ -0,0 +1,8 @@
+var valor1 = prompt("Informe o primeiro valor:");
+var valor2 = prompt("Informe o segundo valor:");
+var transform1 = parseInt(valor1);
+var transform2 = parseInt(valor2);
+var soma = transform1 + transform2;
+
+console.log(soma);
+alert(soma);
